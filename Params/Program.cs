7382ca9ebe6017d3bd86511e9fa506b148d04444
markusanton <markusa070@gmail.com>
2020-10-08 11:25:26 +0300

    Add project files.

---
+++ Params/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParamsSum(5, 5);
+        }
+        public static void ParamsSum(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine(result);
+        }
+        public static void StringFunction()
+        {
+            Console.WriteLine("enter string:");
+            string functionString = Console.ReadLine();
+            Console.WriteLine(functionString);
+        }
+    }
+}
---
+++ Week6Functions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Week6Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //HelloUser();
+            NumberSum();
+        }
+        public static void HelloUser()
+        {
+            Console.WriteLine("what's ur name");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Hello {userName}");
+        }
+        public static void NumberSum()
+        {
+            Console.WriteLine("number 1");
+            int numOne = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("number 2");
+            int numTwo = Int32.Parse(Console.ReadLine());
+
+            int sum = numOne + numTwo;
+            Console.WriteLine($"result = {sum}");
+        }
+    }
+}
